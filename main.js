e0c98bfb9fbe4c7b0c922beb13999073a6f49a37
Jesse Soldat <jsoldat@hotmail.com>
2015-12-25 10:15:10 -0500

    users can rate their favorite magazine cover

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
 
 var images = document.getElementById('imgArray');
+var count = document.getElementById('count');
 var rating = document.getElementById('rating');
 var btn = document.getElementById('btn');
 var range = document.getElementById('range');
-var array = ['cat','dog','pig'];
+var array = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg"];
+var countNum = ['3','2','1'];
 var i = 0;
+var c = 0;
 
-images.innerHTML = array[0];
+  images.setAttribute("src", array[0]);
+
 
 
 function next(){
   i++;
-  images.innerHTML = array[i];
+  // images.innerHTML = array[i];
+  images.setAttribute("src", array[i]);
+
   
     if(i >= array.length) {
       i = 0;
-      images.innerHTML = "You have rated all of the images. Click to start over."
+      images.setAttribute("src", "img/play.png");
       i = -1;
       console.log(range.value);
       btn.innerHTML = "Try Again";
       btn.setAttribute("disabled", true);
 
+      setInterval(function(){
+        count.innerHTML = countNum[c];
+        c++;
+      }, 1000);
+
       setTimeout(function(){ 
         location.reload(); 
+
       }, 3000);
       
       
