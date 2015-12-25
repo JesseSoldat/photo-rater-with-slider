b1d7c222c93a408abf79a6ae1341d010fc54432f
Jesse Soldat <jsoldat@hotmail.com>
2015-12-25 09:18:45 -0500

    basic rating of an image array complete

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+
+var images = document.getElementById('imgArray');
 var rating = document.getElementById('rating');
 var btn = document.getElementById('btn');
 var range = document.getElementById('range');
+var array = ['cat','dog','pig'];
+var i = 0;
+images.innerHTML = 'cat';
+
+function next(){
+  i++;
+  images.innerHTML = array[i];
+  
+    if(i >= array.length) {
+      i = 0;
+      images.innerHTML = "You have rated all of the images. Click to start over."
+      i = -1;
+    }
+}
+
 
 btn.addEventListener('click', submit);
 
@@ -9,6 +26,6 @@ var sliderChange = function (value) {
   rating.innerHTML = value;
 }
 var submit = function() {
-  
  console.log(range.value);
+ next();
 }
