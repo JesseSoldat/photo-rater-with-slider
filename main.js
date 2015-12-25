3d3092f19a009c26d16f2560fa7f638142a94fdf
Jesse Soldat <jsoldat@hotmail.com>
2015-12-25 12:33:56 -0500

    after rating a magazine the rating is written to the page

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ var count = document.getElementById('count');
 var rating = document.getElementById('rating');
 var btn = document.getElementById('btn');
 var range = document.getElementById('range');
+var anchor = document.getElementById('anchor');
+
 var array = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg","img/06.jpg","img/07.jpg"];
+
 var countNum = ['5','4','3','2','1'];
 var i = 0;
 var c = 0;
 
-  images.setAttribute("src", array[0]);
+  images.setAttribute("src", "img/01.jpg");
 
 
 
 function next(){
   i++;
-  // images.innerHTML = array[i];
-  images.setAttribute("src", array[i]);
 
+  images.setAttribute("src", array[i]);
   
     if(i >= array.length) {
       i = 0;
@@ -30,17 +32,15 @@ function next(){
       setInterval(function(){
         count.innerHTML = countNum[c];
         c++;
-      }, 5000);
+      }, 1000);
 
       setTimeout(function(){ 
         location.reload(); 
 
-      }, 3000);
-      
-      
+      }, 5000); 
       return
     }
-    console.log(range.value);
+    
 }
 
 
@@ -49,8 +49,19 @@ btn.addEventListener('click', submit);
 var sliderChange = function (value) {
 
   rating.innerHTML = value;
+
 }
 var submit = function() {
  
+    console.log(range.value);
+    console.log(images.src);
+    var imgName = images.src.slice(-6, -4);
+    console.log(imgName);
+    var element = document.createElement('h3');
+    element.setAttribute("style", "margin-top: 20px; font-size:2em;");
+    var text = document.createTextNode(imgName+": "+ range.value);
+    element.appendChild(text);
+    anchor.appendChild(element);
+
  next();
 }
